a39b3eabc15f578bed16295833b8da83d801b466
Eduardo Freitas De Abreu <95862044+efabreu@users.noreply.github.com>
2023-09-22 09:32:12 -0300

    feat-tela_de_inicio

--- app/src/main/java/br/com/efabreu/portifolioefabreu/fragments/HomeFragment.kt
+++ app/src/main/java/br/com/efabreu/portifolioefabreu/fragments/HomeFragment.kt
@@ -5,22 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import br.com.efabreu.portifolioefabreu.R
+import com.mukesh.MarkDown
+import java.net.URL
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    lateinit var markdownView : ComposeView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +40,34 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupView(view)
+        setupMarkdownView()
+
+    }
+
+    private fun setupView(view: View) {
+        markdownView = view.findViewById(R.id.markdown)
+    }
+
+    private fun setupMarkdownView() {
+        markdownView.apply {
+            // Dispose of the Composition when the view's LifecycleOwner is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                // In Compose world
+                MaterialTheme {
+                    MarkDown(
+                        url = URL("https://raw.githubusercontent.com/efabreu/AppPortifolio/main/README.md"),
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+            }
+        }
     }
 
     companion object {
