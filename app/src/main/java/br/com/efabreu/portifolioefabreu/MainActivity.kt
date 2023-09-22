53321a22539943264588d31aaf2f611b49e58853
Eduardo Freitas De Abreu <95862044+efabreu@users.noreply.github.com>
2023-09-22 11:31:30 -0300

    style: main_activity

--- app/src/main/java/br/com/efabreu/portifolioefabreu/MainActivity.kt
+++ app/src/main/java/br/com/efabreu/portifolioefabreu/MainActivity.kt
@@ -2,11 +2,12 @@ package br.com.efabreu.portifolioefabreu
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.Window
-import android.widget.Toast
+import android.view.WindowManager
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import br.com.efabreu.portifolioefabreu.fragments.GithubFragment
@@ -15,20 +16,34 @@ import br.com.efabreu.portifolioefabreu.fragments.LinkedInFragment
 import com.google.android.material.navigation.NavigationView
 
 
-
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toolbar :Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        setStatusbarColor()
+        setupDrawer(savedInstanceState)
+
+    }
+
+    private fun setupDrawer(savedInstanceState: Bundle?) {
+        drawerLayout = findViewById(R.id.drawer_layout)
+        toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+        supportActionBar?.title = "Home"
         val navigationView = findViewById<NavigationView>(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this)
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.open_nav,
+            R.string.close_nav
+        )
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         if (savedInstanceState == null) {
@@ -37,14 +52,34 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             navigationView.setCheckedItem(R.id.nav_home)
         }
     }
+
+    private fun setStatusbarColor() {
+        val window = window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        val color = ContextCompat.getColor(
+            this,
+            R.color.green
+        )
+        window.statusBarColor = color
+    }
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.nav_home -> supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, HomeFragment()).commit()
-            R.id.nav_linkedin -> supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, LinkedInFragment()).commit()
-            R.id.nav_github -> supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, GithubFragment()).commit()
+            R.id.nav_home -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, HomeFragment()).commit()
+                supportActionBar?.title = "Home"
+            }
+            R.id.nav_linkedin -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, LinkedInFragment()).commit()
+                supportActionBar?.title = "LinkedIn"
+            }
+            R.id.nav_github -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, GithubFragment()).commit()
+                supportActionBar?.title = "Github"
+            }
         }
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
